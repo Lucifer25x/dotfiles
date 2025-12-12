5e22ff36d249dd9323cfd0400012cdade3a4e665
Lucifer25x <lucifer25x@protonmail.com>
2025-12-12 20:28:06 +0400

    Add cool-autostart for dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -20,6 +20,15 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"sh", "-c", "~/.fehbg", NULL,
+	"dunst", NULL,
+	"picom", NULL,
+	"xinput", "set-prop", "ELAN1203:00 04F3:307A Touchpad", "libinput Natural Scrolling Enabled", "1", NULL,
+	"setxkbmap", "-layout", "us,az", "-option", "grp:win_space_toggle", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
